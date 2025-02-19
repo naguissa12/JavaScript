a63bc71cecec8fd906d983f0a206134cc70578ec
naguissa12 <campos.ribeiro.maria@escola.pr.gov.br>
2025-02-19 09:11:23 -0300

    Add files via upload

---
+++ Maria Vitória  3A/area.js
@@ -0,0 +1,5 @@
+const altura= 7
+const largura= 8
+
+operacaoMatematica=(altura*largura)
+console.log(operacaoMatematica)
---
+++ Maria Vitória  3A/idade.js
@@ -0,0 +1,6 @@
+const anoatual =2025;
+const anonascimento =2008;
+
+operacaoMatematica =anoatual-anonascimento
+
+console.log(operacaoMatematica)
